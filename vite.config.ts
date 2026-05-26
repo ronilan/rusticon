15a68184e44316577706976f173963fec77cc111
Ron Ilan <ronilan.webdev@gmail.com>
2026-05-25 19:37:37 -0700

    Incredible from repo. WASM build. Config changes.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
 import { cargoMetadata } from "./scripts/vite-plugin-cargo-metadata";
 
 export default defineConfig({
   root: "web",
   base: "./",
-  plugins: [wasm(), topLevelAwait(), cargoMetadata()],
+  // Removed topLevelAwait() completely to fix the Rollup build error
+  plugins: [wasm(), cargoMetadata()],
   server: {
     host: "0.0.0.0",
     port: 5173,
@@ -15,7 +15,8 @@ export default defineConfig({
     },
   },
   build: {
-    target: "esnext",
+    // "esnext" instructs Vite to preserve native top-level await out-of-the-box
+    target: "esnext", 
     outDir: "../docs",
     emptyOutDir: true,
   },
